b3f453dd3881944922405957b1e845af10afdd74
egdg4587 <egdg7777@gmail.com>
2024-05-31 10:57:50 +0900

    그림

--- src/main/java/org/example/loginapp/shop/Shop.java
+++ src/main/java/org/example/loginapp/shop/Shop.java
@@ -1,4 +1,30 @@
 package org.example.loginapp.shop;
 
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@Getter
+@Entity
+@Table(name = "shop_tb")
 public class Shop {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+
+    @Column(unique = true)
+    private String name; // 상품명
+    private String price; // 상품가격
+    private Integer qty; // 재고
+
+    @Builder
+    public Shop(Integer id, String name, String price, Integer qty) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.qty = qty;
+    }
 }
--- src/main/java/org/example/loginapp/user/User.java
+++ src/main/java/org/example/loginapp/user/User.java
@@ -1,4 +1,34 @@
 package org.example.loginapp.user;
 
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@Getter
+@Entity
+@Table(name = "user_tb")
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+
+    @Column(unique = true)
+    private String username;
+    private String password;
+    private String email;
+
+    // 여기 값이 들어왔다? 그럼 오어스를 사용한것
+    private String provider; // facebook, kakao, apple, naver
+
+    @Builder
+    public User(Integer id, String username, String password, String email, String provider) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.email = email;
+        this.provider = provider;
+    }
 }
